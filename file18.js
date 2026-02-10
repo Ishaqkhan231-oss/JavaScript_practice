154b447c9a37355d9bd0713193aa50b0afd2ec13
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-10 00:00:07 -0800

    Add files via upload

---
+++ file18.js
@@ -0,0 +1,38 @@
+// if
+// else if
+// else if
+// else if
+// else 
+
+/*
+ let tempInDegree =22;
+
+ if(tempInDegree < 0){
+    console.log("Extremly cold outside");
+ }else if(tempInDegree < 16){
+    console.log("It is cold outside");
+ }else if(tempInDegree < 25){
+    console.log("Wheather is okay");
+ }else if(tempInDegree < 35){
+    console.log("lets go outside");
+ }else if(tempInDegree < 45){
+    console.log("turn on the Ac");
+ }else {
+    console.log("too hot");
+ }
+ console.log("Rahat")
+ */
+
+ let tempInDegree = 0;
+ if(tempInDegree > 40){
+    console.log("too hot");
+ }else if(tempInDegree > 30){
+    console.log("let's go for swim")
+ }else if(tempInDegree > 20){
+    console.log("weather is okay");
+ }else if(tempInDegree > 10){
+    console.log("it is very cold today outside")
+ }else {
+    console.log("extreml cold");
+ }
+ console.log("Rahat");

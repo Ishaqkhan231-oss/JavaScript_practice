fa8e6ac0ccedb18b08fb0de4247a6e31837f7b28
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-11 08:51:27 -0800

    Add files via upload

---
+++ file21.js
@@ -0,0 +1,18 @@
+// while loop example 
+// let num = 10;
+total = 0;
+
+let i = 0;
+
+
+while (i <= 10) {
+    total = total + i;
+    i++;
+}
+
+console.log(total);
+// Doing the same work with maths farmula this 
+// one is fast 
+// let n = 10;
+// let total = (n*(n+1))/2;
+// console.log(total);

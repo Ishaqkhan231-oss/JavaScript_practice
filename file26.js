6de5e21de1426b0081222c5cb152c271b77ccea2
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-12 08:55:53 -0800

    Add files via upload

---
+++ file26.js
@@ -0,0 +1,25 @@
+// intro to Array
+// refrence types 
+// how to create arrays 
+
+ let fruits = ["apple","mango","grapes"];
+ let number = [1,2,3,4];
+ let mixed = [1,2,3, "string",null,undefined];
+ console.log(mixed);
+ console.log(number);
+ console.log(fruits[1]);//by index
+
+//  let fruits = ["apple","mango","grapes"];
+ let obj = {}; // object literal
+//  console.log(fruits);
+//  fruits[1] = "banana";
+//  console.log(fruits);
+// array is a refrence type ,every refrecnce type
+// is a object ,array is a object in JS
+// console.log(typeof fruits);
+// console.log(typeof obj);
+// console.log(Array.isArray(fruits));
+// console.log(Array.isArray(obj));
+
+
+// arry indexing 

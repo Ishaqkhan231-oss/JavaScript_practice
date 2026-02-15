4e917f546d8b4d7e23b2739863f43a69ae0cdfb4
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-15 09:40:31 -0800

    Add files via upload

---
+++ file31.js
@@ -0,0 +1,7 @@
+// use const for creating arrays 
+const fruits = ["apple","mango"];
+// fruits = ["grapes","pineapple"]; it cant be 
+// assigned caz of const,but in case of let we can 
+// do this
+fruits.push("banana");
+console.log(fruits);

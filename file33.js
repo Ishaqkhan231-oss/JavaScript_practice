15d97c1f0557aa39c416ddc43dc8ad591ec0792b
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-16 10:06:40 -0800

    Add files via upload

---
+++ file33.js
@@ -0,0 +1,13 @@
+// for of loop in array
+const fruits = ["apple", "mango", "grapes","fruit4"];
+const fruits2 = [];//2nd array
+// we can use any other variable like 
+// i or k for fruit and fruits ,but its not convetional 
+// for(let fruit of fruits){
+//     fruits2.push(fruit.toUpperCase());
+// }
+// console.log(fruits2);
+
+for(let i = 0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}

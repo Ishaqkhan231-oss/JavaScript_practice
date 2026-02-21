a6886bdb107ec0ed65f5d4ccab4e729d32aa1c2b
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-21 10:48:01 -0800

    Add files via upload

---
+++ file34.js
@@ -0,0 +1,10 @@
+// for in loop in array
+
+const fruits = ["apple", "mango", "grapes","fruit4"];
+const fruits2 = [];//2nd array
+
+
+for(let index in fruits){
+   fruits2.push(fruits[index].toUpperCase());
+}
+console.log(fruits2);

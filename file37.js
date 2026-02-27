3f19e1c5b75ea8772d5f129b8e464888f86bd093
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-02-27 09:15:55 -0800

    Add files via upload

---
+++ file37.js
@@ -0,0 +1,11 @@
+// diffrence between dot and bracket notaion
+const key = "email";
+
+const person = {
+    name: "Rahat Ishaq", gae: 19,
+   "person hobbies": ["Coding", "Travel", "Sleeping"]
+
+}
+//  console.log(person["person hobbies"]);
+// person[key] = "Ishaq@gmail.com";
+console.log(person);

61b75629deb99b7075ea83ee38cde7e25a0dc18c
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-03-01 09:05:48 -0800

    Add files via upload

---
+++ file40.js
@@ -0,0 +1,23 @@
+// spread operator in case of arrays
+// const array1 = [1,2,3];
+// const array2 = [4,5,6];
+
+// const newArray = [...array1,...array2, 7, 8, 9];
+// const newArray = [..."RIK"];
+// console.log(newArray);
+
+// spread operator in case of objects
+const obj1 = {
+    key1: "value1",
+    key2: "value2",
+}
+const obj2 = {
+    key1: "valueUnique",//used for overriding
+    key3: "value3",
+    key4: "value4",
+}
+
+// const newObject = { ...obj2, ...obj1, key99: "value99" }
+// const newObject = {...["item1" , "item2"]};
+const newObject = {... "Ishaq"};
+console.log(newObject);

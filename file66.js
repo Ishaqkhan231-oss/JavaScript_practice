6e1ae8db0888cbf794581d2be0d54713c2a50265
Ishaqkhan231-oss <kishaqkhan747@gmail.com>
2026-03-31 09:36:51 -0700

    Add files via upload

---
+++ file66.js
@@ -0,0 +1,30 @@
+// iterables 
+// e.g: string, array are iterable
+// const firstName = "Ishat";
+// for(let char of firstName){
+    // console.log(char);
+// } 
+
+
+// const items = ['item1','item2','item3'];
+// for(let item of items){
+//     console.log(item);
+// }
+
+// objedt is not iterable
+// const users = {'key1': 'value1', 'key2': 'value'}
+// for(let item of users){
+//     console.log(item);
+// }
+
+// array like object
+// jinke pass length property hoti ha
+// our jinko hum index se access kar sakte hai
+//  exaple :- string
+
+const firstName = "Rahat";
+console.log(firstName.length);
+console.log(firstName[0]);
+
+
+
